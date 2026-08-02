400ab4865ebfdc7dd9b39a117f57a62dfd1c8c8f
Mohak Bhatia <bhatiamohak1702@gmail.com>
2026-08-02 18:54:57 +0530

    cicd changes

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -2,13 +2,19 @@ package TestComponents;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -23,14 +29,64 @@ public class BaseTest {
 	public AbstractComponents abstractComponents;
 	
 	public WebDriver Initializer() throws IOException {
-//		if(System.getProperty("browser").equalsIgnoreCase("Chrome"))
-//		{
-//			driver=new ChromeDriver();
-//		}
-//		else if (System.getProperty("browser").equalsIgnoreCase("Edge")) {
-//			driver=new EdgeDriver();
-//		}
-		driver=new ChromeDriver();
+		if(System.getProperty("browser").equalsIgnoreCase("Chrome"))
+		{
+			
+			WebDriverManager.chromedriver().setup();
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--window-size=1920,1080",
+                "--remote-allow-origins=*"
+            );
+            String chromeBinary = System.getProperty("chromeBinary", "/usr/bin/google-chrome");
+            chromeOptions.setBinary(chromeBinary);
+
+            chromeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+            chromeOptions.setExperimentalOption("useAutomationExtension", false);
+
+            ChromeDriverService chromeService = new ChromeDriverService.Builder()
+                .usingAnyFreePort()
+                .withVerbose(true)
+                .withLogFile(new File("chromedriver.log"))
+                .build();
+
+            driver = new ChromeDriver(chromeService, chromeOptions);
+            
+		}
+		else if (System.getProperty("browser").equalsIgnoreCase("Edge")) {
+			
+            WebDriverManager.edgedriver().setup();
+
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--window-size=1920,1080",
+                "--remote-allow-origins=*"
+            );
+            // Allow binary override from CI if needed
+            String edgeBinary = System.getProperty("edgeBinary", "/usr/bin/microsoft-edge");
+            edgeOptions.setBinary(edgeBinary);
+
+            edgeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+            edgeOptions.setExperimentalOption("useAutomationExtension", false);
+
+            EdgeDriverService edgeService = new EdgeDriverService.Builder()
+                .usingAnyFreePort()
+                .withVerbose(true)
+                .withLogFile(new File("edgedriver.log"))
+                .build();
+
+            driver = new EdgeDriver(edgeService, edgeOptions);
+		}
+		
 		return driver;
 	}
 	
